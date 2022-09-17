47af3678b990154e2f3020efb7ed81bd5bba0e27
r-southworth <rachael.southworth@datagrove.com>
2022-09-17 18:20:10 -0400

    Calculate upper and lower bits

--- ef.dart
+++ ef.dart
@@ -1,8 +1,15 @@
 import 'dart:math';
+import 'dart:typed_data';
 
 void main() {
-  print(m);
+  print(upperBits);
 }
 
 var values = [2, 3, 5, 7, 11, 13, 24];
 var encodeBits = (log(values[(values.length - 1)]) / (log(2))).ceil();
+var upperBits = (log(values.length) / log(2)).ceil();
+var lowerBits = encodeBits - upperBits;
+
+efCompress() {
+  //sort list in ascending order
+}
