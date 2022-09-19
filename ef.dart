ffe36fa4061ffc6de4d25fcb286cb79672023e85
r-southworth <rachael.southworth@datagrove.com>
2022-09-19 12:29:42 -0400

    Create Lower Bits List

--- ef.dart
+++ ef.dart
@@ -2,14 +2,37 @@ import 'dart:math';
 import 'dart:typed_data';
 
 void main() {
-  print(upperBits);
+  print(efCompress(values));
 }
 
 var values = [2, 3, 5, 7, 11, 13, 24];
-var encodeBits = (log(values[(values.length - 1)]) / (log(2))).ceil();
-var upperBits = (log(values.length) / log(2)).ceil();
-var lowerBits = encodeBits - upperBits;
 
-efCompress() {
+efCompress(List<int> input) {
   //sort list in ascending order
+  input.sort();
+
+  //check for negative numbers
+  if (input[0].isNegative) {
+    return ("EF encoding does not support negative values");
+  }
+
+  //calculate length of EF dictionary
+  var encodeBits = (log(values[(values.length - 1)]) / (log(2))).ceil();
+
+  //calculate the number of upper bits
+  var upperBits = (log(values.length) / log(2)).ceil();
+
+  //calculate the number of lower bits
+  var lowerBits = encodeBits - upperBits;
+
+  //create lower bits
+  String lowerBitsList = '';
+
+  for (int i = 0; i < values.length; i++) {
+    String allBits = input[i].toRadixString(2);
+    String lowBits = allBits.substring(allBits.length - lowerBits);
+    lowerBitsList += lowBits;
+  }
+
+  return lowerBitsList;
 }
